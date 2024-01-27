d75dffbae661fc3b133be93b2993f25e3a9dbf38
Gustavo <Gu.almeidan2007@gmail.com>
2024-01-27 04:09:29 -0300

    exercicio v2

--- aula 3/ex/index.js
+++ aula 3/ex/index.js
@@ -1,4 +1,5 @@
 let nome = "Gustavo Almeida"
 let hora = 3
 
-console.log ('')
+console.log ('olá, meu nome é', nome, 'e estou estudando Javascript ás', hora, 'horas da manhã') 
+// obs: fiz mais doq era pra fazer, não era nem pra criar variáveis
