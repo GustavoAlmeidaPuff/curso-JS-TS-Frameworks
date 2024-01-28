10abff9eb49aaf6e23f7b6ebe016819fab4318d1
Gustavo <Gu.almeidan2007@gmail.com>
2024-01-28 10:47:48 -0300

    aula 8 midlle

---
+++ aula 8/index.js
@@ -0,0 +1,6 @@
+nome = prompt('me diga seu nome')
+sobrenome = prompt('me diga seu sobrenome')
+idade = prompt('quantos anos tem?')
+peso = prompt('quanto você pesa?')
+altura = prompt('qual é sua altura')
+
