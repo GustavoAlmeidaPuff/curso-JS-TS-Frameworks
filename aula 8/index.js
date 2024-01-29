319773894e7176906c0db6dbbb999bcb431eea82
Gustavo <Gu.almeidan2007@gmail.com>
2024-01-29 06:55:03 -0300

    aula 8 finish

--- aula 8/index.js
+++ aula 8/index.js
@@ -1,6 +1,10 @@
-nome = prompt('me diga seu nome')
-sobrenome = prompt('me diga seu sobrenome')
-idade = prompt('quantos anos tem?')
-peso = prompt('quanto você pesa?')
-altura = prompt('qual é sua altura')
+let nome = prompt('me diga seu nome')
+let sobrenome = prompt('me diga seu sobrenome')
+let idade = prompt('quantos anos tem?')
+let peso = prompt('quanto você pesa?')
+let altura = prompt('qual é sua altura')
 
+let imc = (peso / (altura * altura))
+anoDeNascimento = 2024 - idade
+
+console.log (nome, sobrenome, ' tem ', idade, ' pesa ', peso, ' tem ', altura, ' de altura, seu IMC é de ', imc,' e nasceu em ', anoDeNascimento)
